c1b87b568e6654556dd332bf2aa08c3dfcc23d60
Grizzly <dydrhks02@naver.com>
2023-03-28 18:08:22 +0900

    feat: start project

---
+++ 한글타이핑/한글타이핑/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  한글타이핑
+//
+//  Created by 임 용관 on 2023/03/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 한글타이핑/한글타이핑/_____________App.swift
@@ -0,0 +1,17 @@
+//
+//  _____________App.swift
+//  한글타이핑
+//
+//  Created by 임 용관 on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct _____________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
